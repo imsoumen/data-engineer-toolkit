4424210e42a1712cc22c30d31c1ccfe166bc95f1
Soumen Saha <43337272+imsoumen@users.noreply.github.com>
2025-01-27 00:31:45 +0530

    SQL Solution

--- Problem Solving/Exchange Seats/solution.sql
+++ Problem Solving/Exchange Seats/solution.sql
@@ -1,2 +1,19 @@
 
 select * from seats;
+
+WITH CTE AS
+(
+    SELECT
+        id,
+        student,
+        row_number() over () as rn
+    FROM seats
+    ORDER BY id
+)
+SELECT
+    *,
+    CASE WHEN MOD(rn, 2) = 0 THEN LAG(id) OVER (ORDER BY rn)
+         WHEN MOD(rn, 2) = 1 THEN COALESCE(LEAD(id) OVER (ORDER BY rn), id)
+    END as new_id
+FROM CTE;
+
